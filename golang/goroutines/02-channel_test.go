cc3513d416a66f893d6a4d5dd54d6ee8fbfe2b73
helmimuzkr <mhelmimuzakir@gmail.com>
2023-03-16 01:39:57 +0700

    learn-goroutine: blocking channel

--- golang/goroutines/02-channel_test.go
+++ golang/goroutines/02-channel_test.go
@@ -1,4 +1,4 @@
-package main
+package goroutine
 
 import (
 	"fmt"
@@ -8,7 +8,7 @@ import (
 func sum(x, y int, c chan int) {
 	data := x + y
 
-	// send data to channel
+	// send data value to channel
 	c <- data
 }
 
@@ -21,8 +21,40 @@ func TestChan(t *testing.T) {
 
 	go sum(1, 2, channel)
 
-	// send data in channel to result
-	result := <- channel 
+	// recieve value from channel and store to result
+	result := <-channel
 	fmt.Println(result) // 3
-	
+
+}
+
+// Di Channel terdapat pengirim dan penerima, biasanya pengirim dan penerima ini adalah goroutine yang berbeda.
+// Ketika proses pengiriman atau penerimaan value goroutine akan terkunci
+// hingga sisi yang lain siap untuk mengirim atau menerima.
+func TestBlocking(t *testing.T) {
+	ch := make(chan string)
+
+	// Terlihat disini string "helmi" dikirim ke ch.
+	ch <- "helmi"
+
+	// Lalu, tidak ada variable yang menerima value yang ada di dalam ch.
+	// Hal ini akan menyebabkan aplikasi terkunci atau stuck.
+
+	fmt.Println("line ini tidak akan muncul ke layar karena deadlock/block")
+}
+
+func TestNonBlock(t *testing.T) {
+	ch := make(chan string)
+
+	// Anonymous func ini menerima data dari ch.
+	go func() {
+		// Data dikirimkan ke ch
+		ch <- "helmi"
+	}()
+
+	// Pada proses penerimaan data dari ch, maka akan terjadi blocking.
+	// Karena msgChannel akan menunggu data yang dikirim ke ch.
+	msgChannel := <-ch
+	fmt.Println(msgChannel)
+
+	fmt.Println("line ini berhasil muncul")
 }
