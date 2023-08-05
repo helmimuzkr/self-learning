29601559331f99722ef9d158f719fc9bb43258cc
helmimuzkr <mhelmimuzakir@gmail.com>
2023-08-05 12:23:41 +0700

    spring-core: learning spring boot application

---
+++ java/spring-core/spring-core/src/main/java/com/playground/springcore/common/config/AwareConfig.java
@@ -0,0 +1,7 @@
+package com.playground.springcore.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.playground.springcore.aware")
+public class AwareConfig {
+}
